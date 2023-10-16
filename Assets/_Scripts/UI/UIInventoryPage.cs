457179124cf38d91f757fb66d7780ad1b07b3d0f
Sedjex <kathshem@gmail.com>
2023-10-16 17:50:49 +0300

    Actions

--- Assets/_Scripts/UI/UIInventoryPage.cs
+++ Assets/_Scripts/UI/UIInventoryPage.cs
@@ -15,7 +15,37 @@ public class UIInventoryPage : MonoBehaviour
       UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
       uiItem.transform.SetParent(contentPanel);
       listOfUIItems.Add(uiItem);
+      uiItem.OnItemClicked += HandleItemSelection;
+      uiItem.OnItemBeginDrag += HandleBeginDrag;
+      uiItem.OnItemDroppedOn += HandleSwap;
+      uiItem.OnItemEndDrag += HandleEndDrag;
+      uiItem.OnRightMouseBtnClick += HandleShowItemActions;
     }
+  }
+
+  private void HandleShowItemActions(UIInventoryItem obj)
+  {
+    
+  }
+
+  private void HandleEndDrag(UIInventoryItem obj)
+  {
+    
+  }
+
+  private void HandleSwap(UIInventoryItem obj)
+  {
+    
+  }
+
+  private void HandleBeginDrag(UIInventoryItem obj)
+  {
+    
+  }
+
+  private void HandleItemSelection(UIInventoryItem obj)
+  {
+    Debug.Log(obj.name);
   }
 
   public void Show()
